594db7cfde5acde8fbefb6293aac384d9fa2c157
ninobot <paulinopose@gmail.com>
2026-02-26 11:50:43 -0300

    nose

---
+++ commands/materias.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const { materias, fechasParciales } = require('../utils/data');
+
+const USUARIOS_FILE = path.join(__dirname, '..', 'usuarios.json');
+
+function loadUsuarios() {
+    if (fs.existsSync(USUARIOS_FILE)) {
+        try {
+            return JSON.parse(fs.readFileSync(USUARIOS_FILE, 'utf8'));
+        } catch (err) {
+            console.error('Error reading usuarios.json:', err);
+            return {};
+        }
+    }
+    return {};
+}
+
+
+module.exports = async (message) => {
+    const usuarios = loadUsuarios();
+    const userId = message.author.id;
+
+    if (!usuarios[userId] || !usuarios[userId].materias || usuarios[userId].materias.length === 0) {
+        message.reply("No estás matriculado en ninguna materia. Usa ^matricularse {CODIGO} para agregar materias.");
+        return;
+    }
+    const userMaterias = usuarios[userId].materias;
+    
+}
